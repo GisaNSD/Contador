6f2f59e434066f68c617440ffaacec08bb9f22f7
Giselle <gisi_tina4@hotmail.com>
2020-12-22 15:14:06 +0100

    Cuenta atras casi listo

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 alert('If you are epileptic, pay attention and DONT YOU DARE TO TOUCH THE BUTTON');
+document.getElementById('Nightking').style.display = 'none';
+
 
 function countdown() {
-   
+    
     var backgroundColor = setInterval(setColor, 300);
-   
-    document.getElementById("warning").disabled = true;
     
-    let secs= 10;
-   
+    let secs= 3;
+    
     var downloadTimer = setInterval( function() {
-    
         secs--;
         document.getElementById("warning").textContent = secs;
-            
-        if(secs <= 0){
+        
+        if(secs == 0){
+            winterIsComing();
+            document.getElementById('Nightking').style.display = 'block';
+            document.getElementById('warning').style.display = 'none';
             clearInterval(backgroundColor);
-            clearInterval(downloadTimer);}
-        } ,1000); }
+            clearInterval(downloadTimer);
+        }
+        
+        document.getElementById("warning").disabled = true;
+    } ,1000);
+    
+}
 
+function winterIsComing(){
+    
+        // const buttonToImg= document.querySelector("warning");
+        // const newInput= document.createElement("img");
+        // newInput.value= "https://i.pinimg.com/originals/28/9d/64/289d6444052720ffc35edd9f9006713f.gif";
 
+    document.getElementById("title").innerHTML = "WINTER IS COMING";
+    // buttonToImg.replaceWith('newInput'>);  
+};
 
 function setColor() {
         var x = document.body;
-        x.style.backgroundColor = x.style.backgroundColor == "black" ? "red" : "black";
+        x.style.backgroundColor = x.style.backgroundColor == "red" ? "black" : "red";
       }
